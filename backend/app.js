92bb594bd5d26858cddae472e6a4cda176a93dee
Natalya Gontsa <semina-nd@mail.ru>
2022-10-13 21:42:44 +0300

    fix: оптимизировать работу cors

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,8 @@ const { errors } = require('celebrate');
 
 const allRouters = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
-
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
+const { cors } = require('./middlewares/cors');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // за 15 минут
@@ -24,12 +23,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 const app = express();
 app.listen(3000);
 
+app.use(cors);
+
 app.use(helmet());
 app.use(limiter); // подключаем rate-limiter
 app.use(bodyParser.json());
 app.use(cookieParser());
-
-app.use(cors);
 
 app.use(requestLogger); // Логгер запросов нужно подключить до всех обработчиков роутов
 app.use(allRouters);
